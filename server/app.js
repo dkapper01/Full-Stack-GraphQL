59b779057051ee05dac7390342215305258b57ad
Daniel Kapper <dkapper01@gmail.com>
2018-08-17 09:53:17 -0400

    mongoose.connect to mlab

--- server/app.js
+++ server/app.js
@@ -1,14 +1,29 @@
-const express = require('express'); 
-const graphquHTTP = require('express-graphql');
-const schema = require('./schema/schema');
+const express = require("express");
+const graphquHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
-const app = express(); 
+const app = express();
 
-app.use('/graphql', graphquHTTP({
-  schema,
-  graphiql: true
-}));
+// Connects to database
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log("DB connected"))
+//   .catch(err => console.error(err));
+
+mongoose.connect('mongodb://dan:password1@ds123822.mlab.com:23822/dan');
+mongoose.connection.once('open', () => {
+  console.log("database is connected"); 
+})
+
+app.use(
+  "/graphql",
+  graphquHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
 app.listen(4000, () => {
-  console.log('Now listening to PORT 4000'); 
-})
+  console.log("Now listening to PORT 4000");
+});
