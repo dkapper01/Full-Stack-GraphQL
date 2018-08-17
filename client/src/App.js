77b46ad441a436c7d184220b7aee5c94e71fca6a
Daniel Kapper <dkapper01@gmail.com>
2018-08-17 17:22:09 -0400

    installed and hooked up Apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProider } from 'react-apollo';
 
+
+// components
 import BookList from '../src/components/BookList';
+
+// Setup Apollo client
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
 
 class App extends Component {
   render() {
     return (
+      <ApolloClient client={client}>
       <div className="App">
         <h1>Daniel Kapper</h1>
         <BookList />
